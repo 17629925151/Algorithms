2cf5580fcd10c8c2361c6ad541e10f121570f603
夺命小豆子 <55118273+17629925151@users.noreply.github.com>
2021-11-09 16:44:08 +0800

    算法描述

---
+++ Algotithms/src/main/java/com/algorithms/sort/primary/BucketSort.java
@@ -0,0 +1,12 @@
+package com.algorithms.sort.primary;
+
+
+/*桶排序
+
+* 在额外空间充足的情况下，尽量增大桶的数量
+*
+* 使用的映射函数能够将输入的 N 个数据均匀的分配到 K 个桶中
+
+* */
+public class BucketSort {
+}
---
+++ Algotithms/src/main/java/com/algorithms/sort/primary/RadixSort.java
@@ -0,0 +1,11 @@
+package com.algorithms.sort.primary;
+
+/*基数排序
+
+* 将整数按位数切割成不同的数字，然后按每个位数分别比较。
+*
+* 由于整数也可以表达字符串（比如名字或日期）和特定格式的浮点数，所以基数排序也不是只能使用于整数。
+*
+*/
+public class RadixSort {
+}
